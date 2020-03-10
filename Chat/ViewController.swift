fd08b85daa7417fc9149ff6a14881aeeac747f6e
Raju <r4ju.riy4nd4@gmail.com>
2020-03-10 22:21:35 +0700

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Jelajah Data Semesta on 10/03/20.
+//  Copyright © 2020 Raju Riyanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
